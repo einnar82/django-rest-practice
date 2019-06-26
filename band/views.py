7a91566f753265028229572f39da0a1b22868bbd
Rannie Ollit <einnar82@gmail.com>
2019-06-26 16:01:20 +0800

    Remove the json parsers and convert to response

--- band/views.py
+++ band/views.py
@@ -4,43 +4,46 @@ from django.views.decorators.csrf import csrf_exempt
 from rest_framework.parsers import JSONParser
 from band.models import Person
 from band.serializers import PersonSerializer
+from rest_framework import status
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
 
 # Create your views here.
+@api_view(['GET', 'POST'])
 @csrf_exempt
 def snippet_list(request):
     if request.method == 'GET':
         person = Person.objects.all()
         serializer = PersonSerializer(person, many=True)
-        return JsonResponse(serializer.data, safe=False)
+        return Response(serializer.data)
 
     elif request.method == 'POST':
-        data = JSONParser().parse(request)
-        serializer = PersonSerializer(data=data)
+        serializer = PersonSerializer(data=request.data)
         if serializer.is_valid():
             serializer.save()
-            return JsonResponse(serializer.data, status=201)
-        return JsonResponse(serializer.errors, status=400)
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
+@api_view(['GET', 'PUT', 'DELETE'])
 @csrf_exempt
 def snippet_detail(request, pk):
     try:
         person = Person.objects.get(pk=pk)
     except Person.DoesNotExist:
-        return HttpResponse(status=404)
+        return Response(status=status.HTTP_404_NOT_FOUND)
 
     if request.method == 'GET':
         serializer = PersonSerializer(person)
-        return JsonResponse(serializer.data)
+        return Response(serializer.data)
 
     elif request.method == 'PUT':
-        data = JSONParser().parse(request)
-        serializer = PersonSerializer(person, data=data)
+        serializer = PersonSerializer(person, data=request.data)
         if serializer.is_valid():
             serializer.save()
-            return JsonResponse(serializer.data)
-        return JsonResponse(serializer.errors, status=400)
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
     elif request.method == 'DELETE':
         person.delete()
-        return HttpResponse(status=204)
+        return Response(status=status.HTTP_204_NO_CONTENT)
